6b28cea4ed96889535859db67f0c700602d8010d
Maksym <150614204+MaksElkapalka@users.noreply.github.com>
2024-06-21 00:05:19 +0200

    conflict resolution

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ async def ban_ips(request: Request, call_next: Callable):
 
 app.include_router(auth.router, prefix="/api")
 app.include_router(users.router, prefix="/api")
-app.include_router(contacts.router, prefix="/api")
+# app.include_router(contacts.router, prefix="/api")
 
 
 @app.on_event("startup")
