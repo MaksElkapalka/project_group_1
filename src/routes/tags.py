17472db41b5ff9f76707683d13eb507bb634c353
Maksym <150614204+MaksElkapalka@users.noreply.github.com>
2024-06-22 00:38:53 +0200

    add tags (#16)

---
+++ src/routes/tags.py
@@ -0,0 +1,118 @@
+from typing import List, Optional
+
+from fastapi import APIRouter, Depends, HTTPException, Path, Query, status
+from fastapi_limiter.depends import RateLimiter
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.database.db import get_db
+from src.entity.models import User
+from src.repository import tags as repositories_tag
+from src.schemas.tag import TagResponse, TagSchema
+from src.services.auth import auth_service
+
+router = APIRouter(prefix="/tags", tags=["tags"])
+
+
+@router.get(
+    "/",
+    response_model=List[TagResponse],
+    dependencies=[Depends(RateLimiter(times=5, seconds=60))],
+)
+async def get_tags(
+    limit: int = Query(default=10, ge=10, le=500),
+    offset: int = Query(default=0, ge=0),
+    db: AsyncSession = Depends(get_db),
+    user: User = Depends(auth_service.get_current_user),
+) -> List[TagResponse]:
+    tags = await repositories_tag.get_tags(limit, offset, db, user)
+    return tags
+
+
+@router.get(
+    "/{tag_id}",
+    response_model=TagResponse,
+    dependencies=[Depends(RateLimiter(times=5, seconds=60))],
+)
+async def get_tags(
+    tag_id: int = Path(ge=1),
+    db: AsyncSession = Depends(get_db),
+    user: User = Depends(auth_service.get_current_user),
+) -> TagResponse:
+
+    tag = await repositories_tag.get_tag(tag_id, db, user)
+    if tag is None:
+        raise HTTPException(
+            status_code=404,
+            detail=f"tag with id {tag_id} not found",
+        )
+    return tag
+
+@router.post(
+    "/",
+    response_model=TagResponse,
+    status_code=status.HTTP_201_CREATED,
+    dependencies=[Depends(RateLimiter(times=5, seconds=60))],
+)
+async def create_tag(
+    body: TagSchema,
+    db: AsyncSession = Depends(get_db),
+    user: User = Depends(auth_service.get_current_user),
+) -> TagResponse:
+    tag = await repositories_tag.create_tag(body, db, user)
+    return tag
+
+
+@router.put(
+    "/{tag_id}",
+    response_model=TagResponse,
+    dependencies=[Depends(RateLimiter(times=5, seconds=60))],
+)
+
+@router.delete(
+    "/{tag_id}",
+    response_model=TagResponse,
+    dependencies=[Depends(RateLimiter(times=5, seconds=60))],
+)
+async def delete_tag(
+    tag_id: int = Path(ge=1),
+    db: AsyncSession = Depends(get_db),
+    user: User = Depends(auth_service.get_current_user),
+) -> TagResponse:
+    tag = await repositories_tag.delete_tag(tag_id, db, user)
+    if tag is None:
+        raise HTTPException(
+            status_code=404,
+            detail=f"Tag with id {tag_id} not found",
+        )
+    return tag
+
+
+@router.get(
+    "/search/",
+    response_model=List[TagResponse],
+    dependencies=[Depends(RateLimiter(times=5, seconds=60))],
+)
+async def search_tags(
+    first_name: Optional[str] = None,
+    last_name: Optional[str] = None,
+    email: Optional[str] = None,
+    db: AsyncSession = Depends(get_db),
+    user: User = Depends(auth_service.get_current_user),
+) -> List[TagResponse]:
+    tags = await repositories_tag.search_tags(
+        first_name, last_name, email, db, user
+    )
+    return tags
+
+
+@router.get(
+    "/birthdays/",
+    response_model=List[TagResponse],
+    dependencies=[Depends(RateLimiter(times=5, seconds=60))],
+)
+async def get_birthdays(
+    db: AsyncSession = Depends(get_db),
+    user: User = Depends(auth_service.get_current_user),
+) -> List[TagResponse]:
+    tags = await repositories_tag.get_birthdays(db, user)
+    return tags
