99dc11677a6bc11bd8d416d18bd149c0786f506d
Maksym <150614204+MaksElkapalka@users.noreply.github.com>
2024-06-23 23:57:03 +0200

    Max fix (#28)
    * 01
    
    * 03

--- src/repository/tags.py
+++ src/repository/tags.py
@@ -1,28 +1,66 @@
 from typing import List, Optional
-from sqlalchemy.orm import Session
-from src.entity.models import Tag, Image
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.entity.models import Tag, Image, User
 from src.schemas.tag import TagSchema
 
-def create_tag(body: TagSchema, db: Session) -> Tag:
-    new_tag = Tag(**body.dict())
+
+async def create_tag(body: TagSchema, db: AsyncSession) -> Tag:
+    new_tag = Tag(**body.model_dump(exclude_unset=True))
     db.add(new_tag)
-    db.commit()
-    db.refresh(new_tag)
+    await db.commit()
+    await db.refresh(new_tag)
     return new_tag
 
-def add_tag(photo_id: int, tag_name: str, user_id: int, db: Session) -> Optional[Tag]:
-    photo = db.query(Image).filter(Image.id == photo_id, Image.user_id == user_id).first()
-    if not photo:
-        return None
 
-    tag = db.query(Tag).filter(Tag.name == tag_name).first()
+async def get_tags(skip: int, limit: int, db: AsyncSession) -> List[Tag]:
+    stmt = select(Tag).offset(skip).limit(limit)
+    tags = await db.execute(stmt)
+    return tags.scalars().all()
+
+
+async def get_tag(tag_name: str, db: AsyncSession) -> Optional[Tag]:
+    stmt = select(Tag).filter_by(name=tag_name)
+    tag = await db.execute(stmt)
+    return tag.scalar_one_or_none()
+
+
+async def update_tag(tag_id: int, body: TagSchema, db: AsyncSession):
+    stmt = select(Tag).filter_by(id=tag_id)
+    result = await db.execute(stmt)
+    tag = result.scalar_one_or_none()
     if tag:
-        if tag not in photo.tags:
-            photo.tags.append(tag)
+        tag.name = body.name
+        await db.commit()
+        await db.refresh(tag)
+    return tag
+
+
+async def remove_tag(tag_id: int, db: AsyncSession):
+    stmt = select(Tag).filter_by(id=tag_id)
+    result = await db.execute(stmt)
+    tag = result.scalar_one_or_none()
+    if tag:
+        await db.delete(tag)
+        await db.commit()
+    return tag
+
+
+async def add_tag_for_image(tag_name: str, image_id: int, user: User, db: AsyncSession):
+    stmt = select(Image).filter_by(id=image_id, user=user)
+    result = await db.execute(stmt)
+    image = result.scalar_one_or_none()
+    if image:
+        tag = await get_tag(tag_name, db)
+        if tag:
+            if tag not in image.tags:
+                image.tags.append(tag)
+                db.commit()
+            return tag
+        else:
+            new_tag = await create_tag(TagSchema(name=tag_name), db)
+            image.tags.append(new_tag)
             db.commit()
-        return tag
-    else:
-        new_tag = create_tag(TagSchema(name=tag_name), db)
-        photo.tags.append(new_tag)
-        db.commit()
-        return new_tag
+            return new_tag
+    return image
--- src/routes/tags.py
+++ src/routes/tags.py
@@ -1,53 +1,80 @@
 from typing import List
-from fastapi import APIRouter, HTTPException, Depends, status
-from sqlalchemy.orm import Session
+from fastapi import APIRouter, HTTPException, Depends, Path, status
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.database.db import get_db
 from src.schemas.tag import TagSchema, TagResponse
 from src.repository import tags as repository_tags
-from src.services.auth import auth_service 
+from src.services.auth import auth_service
 from src.entity.models import User
+from src.conf import messages
 
-router = APIRouter(prefix='/tags', tags=["tags"])
+
+router = APIRouter(prefix="/tags", tags=["tags"])
+
+
 
 @router.get("/", response_model=List[TagResponse])
-def read_tags(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    tags = repository_tags.get_tags(skip, limit, db)
+async def read_tags(
+    skip: int = 0, limit: int = 100, db: AsyncSession = Depends(get_db)
+):
+    tags = await repository_tags.get_tags(skip, limit, db)
+    if tags is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=messages.TAG_NOT_FOUND
+        )
     return tags
 
-@router.get("/{tag_id}", response_model=TagResponse)
-def read_tag(tag_id: int, db: Session = Depends(get_db)):
-    tag = repository_tags.get_tag(tag_id, db)
+
+@router.get("/{tag_name}", response_model=TagResponse)
+async def read_tag(tag_name: str, db: AsyncSession = Depends(get_db)):
+    tag = await repository_tags.get_tag(tag_name, db)
     if tag is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Tag not found")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=messages.TAG_NOT_FOUND
+        )
     return tag
 
-@router.post("/", response_model=TagResponse)
-def create_tag(body: TagSchema, db: Session = Depends(get_db)):
-    return repository_tags.create_tag(body, db)
+
+@router.post("/", response_model=TagResponse, status_code=status.HTTP_201_CREATED)
+async def create_tag(body: TagSchema, db: AsyncSession = Depends(get_db)):
+    tag = await repository_tags.create_tag(body, db)
+    return tag
+
 
 @router.put("/{tag_id}", response_model=TagResponse)
-def update_tag(body: TagSchema, tag_id: int, db: Session = Depends(get_db)):
-    tag = repository_tags.update_tag(tag_id, body, db)
+async def update_tag(
+    body: TagSchema, tag_id: int = Path(ge=1), db: AsyncSession = Depends(get_db)
+):
+    tag = await repository_tags.update_tag(tag_id, body, db)
     if tag is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Tag not found")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=messages.TAG_NOT_FOUND
+        )
     return tag
 
-@router.delete("/{tag_id}", response_model=TagResponse)
-def remove_tag(tag_id: int, db: Session = Depends(get_db)):
-    tag = repository_tags.remove_tag(tag_id, db)
+
+@router.delete("/{tag_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def remove_tag(tag_id: int = Path(ge=1), db: AsyncSession = Depends(get_db)):
+    tag = await repository_tags.remove_tag(tag_id, db)
     if tag is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Tag not found")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=messages.TAG_NOT_FOUND
+        )
     return tag
 
-@router.post("/add", response_model=TagResponse)
-def add_tag(
-    photo_id: int,
+
+@router.post("/add", response_model=TagResponse, status_code=status.HTTP_201_CREATED)
+async def add_tag_for_image(
     tag_name: str,
-    db: Session = Depends(get_db),
-    current_user: User = Depends(auth_service.get_current_user)
-  ):
-    tag = repository_tags.add_tag(photo_id, tag_name, current_user.id, db)
+    image_id: int = Path(ge=1),
+    db: AsyncSession = Depends(get_db),
+    user: User = Depends(auth_service.get_current_user),
+):
+    tag = await repository_tags.add_tag_for_image(tag_name, image_id, user, db)
     if tag is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Photo not found or does not belong to the user")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Photo not found or does not belong to the user",
+        )
     return tag
