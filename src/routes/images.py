527d4bac97158073e42a3b3e9faac03e24e99ed8
MykhailoMykhailiuk <151375054+MykhailoMykhailiuk@users.noreply.github.com>
2024-06-26 21:49:27 +0100

    fixed image + set admin access to image routes (#43)
    Co-authored-by: Maksym <150614204+MaksElkapalka@users.noreply.github.com>

--- src/routes/images.py
+++ src/routes/images.py
@@ -1,19 +1,27 @@
 from typing import List
 
-from fastapi import APIRouter, UploadFile, File, Form, Depends, HTTPException, Query
+from fastapi import (
+    APIRouter,
+    UploadFile,
+    File,
+    Form,
+    Depends,
+    HTTPException,
+    Query,
+    status)
 from sqlalchemy.orm import Session
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.entity.models import User, Image
 from src.database.db import get_db
 from src.repository import images as repository_images
-from src.schemas.image import ImageSchema, ImageUpdateSchema, ImageResponse
-from src.services.auth import auth_service
+from src.schemas.image import ImageSchema, ImageUpdateSchema, ImageResponse, ImageCreate
+from src.services.auth import auth_service, role_required
+from src.conf import messages
 
 router = APIRouter(prefix="/images", tags=["images"])
 
-
-@router.post("/upload/", response_model=ImageResponse, status_code=201)
+@router.post("/upload/", response_model=ImageCreate, status_code=201)
 async def upload_image_endpoint(
     file: UploadFile = File(...),
     description: str = Form(...),
@@ -23,30 +31,23 @@ async def upload_image_endpoint(
     result = await repository_images.upload_image(file.file, description, db, user)
     return result
 
-
-@router.put("/update/{image_id}")
+@router.put("/update/{image_id}",
+            response_model=ImageResponse,
+            dependencies=[Depends(auth_service.get_current_active_user),
+                          Depends(role_required(["admin"]))]
+)
 async def update_image_endpoint(
     body: ImageUpdateSchema,
-    image_id: int,
-    db: AsyncSession = Depends(get_db),
-    user: User = Depends(auth_service.get_current_user),
+    image_id: int, 
+    db: AsyncSession = Depends(get_db)
 ):
-    image = await repository_images.update_image(image_id, body, db, user)
+    image = await repository_images.update_image(image_id, body, db)
     if image is None:
-        raise HTTPException(status_code=404, detail="Image not found")
-    return {
-        "message": "Image description successfully updated",
-        "image": image.url,
-        "description": image.description,
-    }
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=messages.IMAGE_NOT_FOUND)
+    return image
 
-
-@router.get("/show/", response_model=list[ImageResponse])
-async def get_all_images(
-    limit: int = Query(10, ge=10, le=500),
-    offset: int = Query(0, ge=0, le=10),
-    db: AsyncSession = Depends(get_db),
-):
+@router.get("/show/", response_model=List[ImageResponse])
+async def get_all_images(limit: int = Query(10, ge=10, le=500), offset: int = Query(0, ge=0, le=10), db: AsyncSession = Depends(get_db)):
     result = await repository_images.get_all_images(limit, offset, db)
     return result
 
@@ -59,19 +60,19 @@ async def get_image(
 ):
     result = await repository_images.get_image(image_id, db, user)
     if result is None:
-        raise HTTPException(status_code=404, detail=f"Image not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=messages.IMAGE_NOT_FOUND)
     return result
 
-
-@router.delete("/{image_id}")
+@router.delete("/{image_id}",
+               dependencies=[Depends(auth_service.get_current_active_user),
+                             Depends(role_required(["admin"]))]
+)
 async def delete_image_endpoint(
     image_id: int,
     db: AsyncSession = Depends(get_db),
-    user: User = Depends(auth_service.get_current_user),
 ):
-    deleted = await repository_images.delete_image(image_id, db, user)
-    if not deleted:
-        raise HTTPException(
-            status_code=403, detail="You do not have permission to delete this image"
-        )
+    image = await repository_images.delete_image(image_id, db)
+    if image is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=messages.IMAGE_NOT_FOUND)
     return {"message": "Image deleted successfully"}
+
